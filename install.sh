7c320ee8fe35fbad51b1b2be6ae09dd581834e10
Ayobami6 <ayobamidele006@gmail.com>
2024-10-30 10:13:54 +0100

    Fix: Correct installation path for sparky binary
    Corrected the path used when copying the `sparky` binary to `/usr/local/bin` during installation.  The previous path was incorrect, preventing successful installation. This ensures the `sparky` command is available in the user's PATH after installation.

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 install () {
     echo "Installing sparky cli"
     git clone https://github.com/Ayobami6/sparky_generate
-    chmod u+x ./bin/sparky
-    sudo cp ./bin/sparky /usr/local/bin
+    chmod u+x ./sparky_generate/bin/sparky
+    sudo cp ./sparky_generate/bin/sparky /usr/local/bin
 
     # clean up
     rm -rf ./sparky_generate
