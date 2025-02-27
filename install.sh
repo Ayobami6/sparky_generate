b6bef790e8b9bf45a8f77b510f4d97ccea89707a
Ayobami6 <ayobamidele006@gmail.com>
2025-02-27 17:40:49 +0100

    feat: Make sparky executable on macOS during installation

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ install () {
     # get the arc name
     os_name=$(uname)
     if [[ $os_name == "Darwin" ]]; then
+        chmod u+x ./sparky_generate/bin/mac/sparky
         sudo cp ./sparky_generate/bin/mac/sparky /usr/local/bin
     elif [[ $os_name == "Linux" ]]; then
         sudo cp ./sparky_generate/bin/sparky /usr/local/bin
